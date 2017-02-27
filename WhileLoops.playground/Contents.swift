00766150df06070e0b947ecf1b2e458a7a593cbd
Joe Hill <joe@jkh3.com>
2017-02-27 18:13:59 -0500

    Add files via upload

---
+++ WhileLoops.playground/Contents.swift
@@ -0,0 +1,67 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var i = 1
+
+while i < 10 {
+	
+	
+	print (i)
+	
+	i = i + 1
+	
+	//  or i += 1
+	
+}
+
+
+ // Challenge - display the first 20 numbers in the 7 times table
+
+var j = 1
+
+while j <= 20 {
+	
+	print (j * 7)
+	
+	j += 1
+}
+
+// 
+
+var array = [7, 23, 98, 1, 0, 763]
+
+i = 0
+
+while i < array.count{
+	
+	array[i] += 1
+	
+	i += 1
+}
+
+print (array)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	
+
+
+
+
+
